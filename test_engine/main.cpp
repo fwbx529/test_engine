69f1925f4800d953187a0067cdb7d2758c05eec2
fwbx529 <suzhaoqi280579550@163.com>
2019-05-28 20:29:44 +0800

    100 balls

--- test_engine/main.cpp
+++ test_engine/main.cpp
@@ -2,11 +2,16 @@
 #include "Scene.h"
 #include <time.h>
 
+float rand1() { return (float)rand() / RAND_MAX; }
+glm::vec3 rand_vec3() { return glm::vec3(rand1(), rand1(), rand1()); }
+
 float aspect;
 void APIENTRY glDebugOutput(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar *message, const void *userParam) { if (id == 131169 || id == 131185 || id == 131218 || id == 131204) return; std::cout << "---------------" << std::endl; std::cout << "Debug message (" << id << "): " << message << std::endl; switch (source) { case GL_DEBUG_SOURCE_API: std::cout << "Source: API"; break; case GL_DEBUG_SOURCE_WINDOW_SYSTEM: std::cout << "Source: Window System"; break; case GL_DEBUG_SOURCE_SHADER_COMPILER: std::cout << "Source: Shader Compiler"; break; case GL_DEBUG_SOURCE_THIRD_PARTY: std::cout << "Source: Third Party"; break; case GL_DEBUG_SOURCE_APPLICATION: std::cout << "Source: Application"; break; case GL_DEBUG_SOURCE_OTHER: std::cout << "Source: Other"; break; } std::cout << std::endl; switch (type) { case GL_DEBUG_TYPE_ERROR: std::cout << "Type: Error"; break; case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: std::cout << "Type: Deprecated Behaviour"; break; case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR: std::cout << "Type: Undefined Behaviour"; break; case GL_DEBUG_TYPE_PORTABILITY: std::cout << "Type: Portability"; break; case GL_DEBUG_TYPE_PERFORMANCE: std::cout << "Type: Performance"; break; case GL_DEBUG_TYPE_MARKER: std::cout << "Type: Marker"; break; case GL_DEBUG_TYPE_PUSH_GROUP: std::cout << "Type: Push Group"; break; case GL_DEBUG_TYPE_POP_GROUP: std::cout << "Type: Pop Group"; break; case GL_DEBUG_TYPE_OTHER: std::cout << "Type: Other"; break; } std::cout << std::endl; switch (severity) { case GL_DEBUG_SEVERITY_HIGH: std::cout << "Severity: high"; break; case GL_DEBUG_SEVERITY_MEDIUM: std::cout << "Severity: medium"; break; case GL_DEBUG_SEVERITY_LOW: std::cout << "Severity: low"; break; case GL_DEBUG_SEVERITY_NOTIFICATION: std::cout << "Severity: notification"; break; } std::cout << std::endl; std::cout << std::endl; }
 
 int main()
 {
+    srand(time(0));
+
     const int width = 800;
     const int height = 600;
     aspect = float(height) / float(width);
@@ -51,9 +56,10 @@ int main()
     };
     room.SetColor(room_color);
     scene.AddCube(room);
-    scene.AddSphere(Sphere(glm::vec3(0, 0, -3), glm::vec3(0.5f), glm::vec3(0, 1, 1)));
-    scene.AddSphere(Sphere(glm::vec3(0, 0.805, -3), glm::vec3(0.3f), glm::vec3(1, 1, 0)));
-    scene.AddSphere(Sphere(glm::vec3(0, 1.21, -3), glm::vec3(0.1f), glm::vec3(1, 0, 1)));
+    for (int i = 0; i < 100; i++)
+    {
+        scene.AddSphere(Sphere(rand_vec3() * 2.0f - 1.0f, glm::vec3(0.2f * rand1()), rand_vec3()));
+    }
 
     while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && !glfwWindowShouldClose(window))
     {
